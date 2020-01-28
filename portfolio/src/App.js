43111680a61780bb418383ddec55b13b0ca3b65d
VaderSteve <vadersteve@VaderSteves-MBP.cfl.rr.com>
2020-01-27 21:26:10 -0500

    Basic header displaying

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AppBar from './Components/AppBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <AppBar />
     </div>
   );
 }
