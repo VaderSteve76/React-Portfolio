fa099d8537534af50c4f0555d7246d503de8c62a
VaderSteve76 <alverson.s@yahoo.com>
2020-02-08 13:47:56 -0500

    TodoApp working, just need styling

--- portfolio/src/Components/TodoApp.js
+++ portfolio/src/Components/TodoApp.js
@@ -39,3 +39,56 @@ function TodoForm({ addTodo }) {
     </form>
   );
 }
+
+function TodoApp() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Do Laundry",
+      isCompleted: false
+    },
+    {
+      text: "Take out trash",
+      isCompleted: false
+    },
+    {
+      text: "Vacuum",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default TodoApp;
