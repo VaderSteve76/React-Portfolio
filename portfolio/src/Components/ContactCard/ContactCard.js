9102764a2b89364b522fb93e9fca122ff3ffe6f6
Steve <alverson.s@yahoo.com>
2020-12-15 20:18:52 -0500

    Added target _blank attribute

--- portfolio/src/Components/ContactCard/ContactCard.js
+++ portfolio/src/Components/ContactCard/ContactCard.js
@@ -6,9 +6,10 @@ export default class Card extends Component {
     return (
       <S.MainCard>
         <a href="mailto:salverson1976@gmail.com">My Email</a><br></br><br></br>
-        <a href="https://docs.google.com/document/d/1LhFzKB7oz46joR0AwfVRU9ruR6lbwJFrvQVb6yu4BgA/edit?usp=sharing">My Resume</a><br></br><br></br>
-        <a href="https://www.linkedin.com/in/steve-alverson-159810131/">LinkedIn</a><br></br><br></br>
-        <a href="https://github.com/VaderSteve76">Github</a>
+        <a href="https://docs.google.com/document/d/1LhFzKB7oz46joR0AwfVRU9ruR6lbwJFrvQVb6yu4BgA/edit?usp=sharing" target="_blank">My Resume</a><br></br><br></br>
+        <a href="https://www.linkedin.com/in/steve-alverson-159810131/" target="_blank">LinkedIn</a><br></br><br></br>
+        <a href="https://github.com/VaderSteve76" target="_blank">Github</a>
+        <a href="https://codepen.io/collection/XWdQqB" target="_blank">Work Examples</a>
       </S.MainCard>
     )
   }
